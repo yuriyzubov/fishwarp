63ceeab7b252b2c6e939e3c6890336454e6c0d8b
yuriyzubov <zubov452@gmail.com>
2026-02-25 14:40:42 -0500

    update xml declaration from lxml- to minidom-style

--- bigstitcher/tests/test_write_dataset_xml.py
+++ bigstitcher/tests/test_write_dataset_xml.py
@@ -29,7 +29,7 @@ SAMPLE_ZGROUPS = [
 
 # Expected XML output
 EXPECTED_XML = """\
-<?xml version='1.0' encoding='UTF-8'?>
+<?xml version="1.0" ?>
 <SpimData version="0.2">
   <BasePath type="relative">.</BasePath>
   <SequenceDescription>
